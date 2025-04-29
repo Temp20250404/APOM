f5af7f440bb22fee8b3d11fa8a0bdad958cabe4b
ManJin12 <hh5570hh@naver.com>
2025-04-29 12:49:14 +0900

    [fix] : 충돌난거 해결

--- APOM/Assets/01. Scripts/UI/UI_InventorySlot.cs
+++ APOM/Assets/01. Scripts/UI/UI_InventorySlot.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
 
-public class UI_InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{
-    private Item item;
-    private UI_InventoryTooltip tooltip;
+//public class UI_InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+//{
+//    private Item item;
+//    private UI_InventoryTooltip tooltip;
 
-    public void Init(Item itemData, UI_InventoryTooltip tooltipRef) 
-    {
-        item = itemData;
-        tooltip = tooltipRef;
-    }
+//    public void Init(Item itemData, UI_InventoryTooltip tooltipRef) 
+//    {
+//        item = itemData;
+//        tooltip = tooltipRef;
+//    }
 
-    public void OnPointerEnterHandler(PointerEventData eventData)
-    {
-        tooltip.ShowTooltip(item, Input.mousePosition);
-    }
+//    public void OnPointerEnterHandler(PointerEventData eventData)
+//    {
+//        tooltip.ShowTooltip(item, Input.mousePosition);
+//    }
 
-    public void OnpointerExit(PointerEventData eventData)
-    {
-        tooltip.HideTooltip();
-    }
-}
+//    public void OnpointerExit(PointerEventData eventData)
+//    {
+//        tooltip.HideTooltip();
+//    }
+//}
