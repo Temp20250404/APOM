b8abcc5ca117326704a13bceadbdadf48164c473
ChinoDaiski <callmeson1@naver.com>
2025-04-30 18:23:26 +0900

    [Feat] LoadingScene 일부 작업 및 패킷 내용 갱신
    - LoadingScene Manager 제작 및 이식 중
    - 테스트를 위해 로그인 플로우 대신 OnConnected 호출시 캐릭터 및 몬스터 생성
    - 서버에 저장하는 플레이어 정보 수정
    - 플레이어, 몬스터 공격 관련 패킷 추가

--- APOM/Assets/01. Scripts/Network/Packet/PacketHandler.cs
+++ APOM/Assets/01. Scripts/Network/Packet/PacketHandler.cs
@@ -197,6 +197,14 @@ class PacketHandler
         player.inputController.ReciveTransformSyncRotation(positionSyncPacket);
     }
 
+    // SC_BOSS_ATTACK 패킷을 처리하는 함수
+    public static void SC_BossAttack(PacketSession session, IMessage packet)
+    {
+        SC_BOSS_ATTACK bossAttackPacket = packet as SC_BOSS_ATTACK;
+
+        // TODO: SC_BossAttack 패킷 처리 로직을 여기에 구현
+    }
+
     // SC_REMOVE_CHARACTER 패킷을 처리하는 함수
     public static void SC_RemoveCharacter(PacketSession session, IMessage packet)
     {
