24c6092231c90da141b6bc9bed0c2f1f62af12f0
ManJin12 <hh5570hh@naver.com>
2025-05-12 20:41:02 +0900

    [Chore] : 스킬 각각 캔버스 설정, 범위 변경

--- APOM/Assets/01. Scripts/UI/MonsterUI/UI_BossCondition.cs
+++ APOM/Assets/01. Scripts/UI/MonsterUI/UI_BossCondition.cs
@@ -54,7 +54,7 @@ public class UI_BossCondition : UI_Popup
 
     public void SetHPFill(SC_BOSS_PHASE packet)
     {
-        hpFill.fillAmount = packet.CurrentHp / packet.MaxHp;
+        //hpFill.fillAmount = packet.CurrentHp / packet.MaxHp;
     }
 
     public void AddBuff()
